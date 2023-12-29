966abef0020f023c07faf2544db60f64ff213a73
Kalyan Reddy Daida <stacksimplify@gmail.com>
2023-12-29 16:53:35 +0530

    Welcome to StackSimplify by Kalyan Reddy Daida

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c3-ec2securitygroups.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c3-ec2securitygroups.tf
@@ -1,14 +1,4 @@
 # Create Security Group - SSH Traffic
-resource "aws_security_group" "vpc-ssh" {
-  name        = "vpc-ssh"
-  description = "Dev VPC SSH"
-  ingress {
-    description = "Allow Port 22"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   egress {
     description = "Allow all ip and ports outbound"    
