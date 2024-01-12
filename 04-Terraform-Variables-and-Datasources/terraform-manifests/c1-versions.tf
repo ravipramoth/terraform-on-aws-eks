cf8d67a8be8aa8caee385541088f94c06b541fa9
ravipramoth <ravipramoth@gmail.com>
2024-01-12 15:21:21 +0530

    testing

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c1-versions.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c1-versions.tf
@@ -1,11 +1,9 @@
 # Terraform Block
 terraform {
-  #required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
-  required_version = ">= 1.6.0" 
+  required_version = ">=1.5.6"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      #version = ">= 4.65"
+      source = "hashicorp/aws"
       version = ">= 5.31"
     }
   }
@@ -13,9 +11,7 @@ terraform {
 
 # Provider Block
 provider "aws" {
-  region  = var.aws_region
+  region = "us-east-1"
+  
 }
-/*
-Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
-$HOME/.aws/credentials
-*/
+
